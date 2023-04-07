321f48812182dfa81503b30c3ff8663df7830a71
YingMingFeng <1176551922@qq.com>
2023-04-08 00:22:35 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/coolweather/android/db/County.java
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -6,11 +6,11 @@ import org.litepal.crud.DataSupport;
  * Created by 沣沣最帅 on 2023/4/7.
  */
 
-public class Country extends DataSupport {
+public class County extends DataSupport {
 
     private int id;
 
-    private String countryName;
+    private String countyName;
 
     private String weatherId;
 
@@ -24,12 +24,12 @@ public class Country extends DataSupport {
         this.id = id;
     }
 
-    public String getCountryName() {
-        return countryName;
+    public String getCountyName() {
+        return countyName;
     }
 
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
+    public void setCountyName(String countryName) {
+        this.countyName = countryName;
     }
 
     public String getWeatherId() {
